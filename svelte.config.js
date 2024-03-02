10c71d02fa9a36535cbf7fdf35edf4686f5f1300
haruto17 <s1902272@sendai-nct.jp>
2024-03-02 14:52:38 +0900

    修正

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
 
-const dev = process.argv.include("dev");
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
 		paths: {
-			base: dev ? '' : '/neumo-calendar'
+			base: process.env.NODE_ENV === "production" ? "/sveltekit-gh-pages" : "",
 		}
-	}
+	},
 };
 
 export default config;
