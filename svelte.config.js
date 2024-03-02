57e2e873fd7028e26eb2c36955c59d5a0e111517
haruto17 <s1902272@sendai-nct.jp>
2024-03-02 14:42:27 +0900

    github pages向けに設定変更

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,14 @@
 import adapter from '@sveltejs/adapter-static';
 
+const dev = process.argv.include("dev");
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		paths: {
+			base: dev ? '' : '/neumo-calendar'
+		}
 	}
 };
 
